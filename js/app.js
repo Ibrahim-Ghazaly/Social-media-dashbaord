9ef5d4c9813331dd18a2e100778d29f47b59c8b2
unknown <ibrahimghazaly88@gmail.com>
2022-09-19 17:41:10 +0300

    social media dashboard

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let socialMediaDashbord=document.querySelector(".social-media-dashbord");
+let toggleButton =document.querySelector(".toggle-btn");
+
+
+
+
+toggleButton.addEventListener("click",()=>{
+
+        socialMediaDashbord.classList.toggle("toggle-dark");
+   
+})
